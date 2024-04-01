02304715c7313b9b7f0d76dbdf378008436b8bf3
alxmart <alxmartinez@gmail.com>
2024-04-01 20:27:58 -0300

    Added AlertDialog with Positive/Negative/Neutral options.

--- app/src/main/java/com/luizafmartinez/at08_lesson7_alertdialog/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/at08_lesson7_alertdialog/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.luizafmartinez.at08_lesson7_alertdialog
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.luizafmartinez.at08_lesson7_alertdialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,4 +27,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-}
+
+    fun save(view: View) {
+        val saveAlert = AlertDialog.Builder(this)
+        saveAlert.setTitle("Save")
+        saveAlert.setMessage("Are you sure you want to save your changes ?")
+
+        saveAlert.setPositiveButton("Yes")
+        { dialogInterface: DialogInterface, _: Int -> Snackbar.make(
+           findViewById(R.id.button),
+           "Saved",Snackbar.LENGTH_LONG).show()}
+
+        saveAlert.setNegativeButton("No")
+        { dialogInterface: DialogInterface, _: Int -> Snackbar.make(
+          findViewById(R.id.button),
+          "Not Saved",Snackbar.LENGTH_LONG).show()}
+
+        saveAlert.setNeutralButton("Remind me later", null)
+
+        saveAlert.show()
+        }
+
+    }
